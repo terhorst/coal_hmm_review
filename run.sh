162516f605c5b69e36e21cbbd2bb4bd5b2820661
Jonathan Terhorst <terhorst@gmail.com>
2018-05-12 14:14:16 -0700

    nersc updates

--- run.sh
+++ run.sh
@@ -1,6 +1,11 @@
 #!/bin/bash -ex
-OUTPUT_DIRECTORY=$HOME/benchmark/output3
+#-SBATCH -C haswell
+#SBATCH -A m2871
+#SBATCH -q premium
+#SBATCH -N 8
 export PYTHONPATH=.
+OUTPUT_DIRECTORY=/project/projectdirs/m2871/terhorst/coal_hmm_review/pipeline.output
+# rm -rf $OUTPUT_DIRECTORY
 mkdir -p $OUTPUT_DIRECTORY
 # luigi --module tasks \
 #      EstimateManyReplicates \
@@ -10,22 +15,21 @@ mkdir -p $OUTPUT_DIRECTORY
 #      --GlobalConfig-output-directory $OUTPUT_DIRECTORY \
 #      --workers 24 --local-scheduler $@
 luigi --module tasks \
-     PlotDical \
-     --PlotDical-N 10 \
-     --PlotDical-seed 1 \
-     --PlotDical-demography migration \
-     --GlobalConfig-n-contigs 1 \
-     --GlobalConfig-chromosome-length 10_000_000 \
+     PlotAllCombined \
+     --PlotAllCombined-N 100 \
+     --PlotAllCombined-n-replicates 1 \
+     --GlobalConfig-n-contigs 10 \
+     --GlobalConfig-chromosome-length 100_000_000 \
      --GlobalConfig-output-directory $OUTPUT_DIRECTORY \
-     --workers 12 --local-scheduler $@
+     --workers 128 $@
 # luigi --module tasks \
-#       EstimateSizeHistorySMC \
-#       --EstimateSizeHistorySMC-seed 1 \
-#       --EstimateSizeHistorySMC-N 10 \
-#       --EstimateSizeHistorySMC-demography migration \
-#       --GlobalConfig-n-contigs 1 \
-#       --GlobalConfig-chromosome-length 10_000_000 \
-#       --GlobalConfig-output-directory $OUTPUT_DIRECTORY \
-#       --workers 24 --local-scheduler $@
+#      PlotDical \
+#      --PlotDical-N 10 \
+#      --PlotDical-seed 1 \
+#      --PlotDical-demography migration \
+#      --GlobalConfig-n-contigs 1 \
+#      --GlobalConfig-chromosome-length 10_000_000 \
+#      --GlobalConfig-output-directory $OUTPUT_DIRECTORY \
+#      --workers 12 --local-scheduler $@
 
 # cp $OUTPUT_DIRECTORY/*/*/*.pdf ~/Dropbox/plots/bench
